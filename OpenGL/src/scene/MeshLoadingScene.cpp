0cf9db8f070b66d62f646bd160e08b78c04f4542
Leonhard <leonhard.olaye-felix@student.manchester.ac.uk>
2026-06-14 19:34:27 +0100

    Revert assimp scene to use backpack.obj model

--- OpenGL/src/scene/MeshLoadingScene.cpp
+++ OpenGL/src/scene/MeshLoadingScene.cpp
@@ -6,7 +6,7 @@ scene::MeshLoadingScene::MeshLoadingScene()
 {
 	DoPreviousInit();
 
-	m_Model = std::make_unique<Model>("res/models/rock/rock.obj");
+	m_Model = std::make_unique<Model>("res/models/backpack/backpack.obj");
 }
 
 scene::MeshLoadingScene::~MeshLoadingScene()
